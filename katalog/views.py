29a8e341a5e7f6f8956a44773fb7664a86461410
maysha.haliza <maysha.haliza@ui.ac.id>
2022-09-14 22:58:27 +0700

    ver1

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
+# views.py katalog tugas 2
 # TODO: Create your views here.
+def show_katalog(request):
+    data_katalog_item = CatalogItem.objects.all()
+    context = {
+    'list_item': data_katalog_item,
+    'name': 'Maysha Haliza Kirana'
+    }
+    return render(request, "katalog.html", context)
+    
